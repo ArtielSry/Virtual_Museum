427b7a162c61094bf9210af8d3e4a1b08413ae33
Soraya Artiel <artielsoraya@gmail.com>
2024-05-25 17:53:41 +0200

    Updates

--- src/components/buttons/Button.jsx
+++
@@ -1,7 +0,0 @@
-import style from './button.module.css';
-
-const Button = ({ text }) => {
-	return <button className={style.button}>{text}</button>;
-};
-
-export default Button;
--- vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-
-// https://vitejs.dev/config/
-export default defineConfig({
-	plugins: [react()],
-	base: 'https://artielsry.github.io/Virtual_Museum/vite-deploy'
-});
